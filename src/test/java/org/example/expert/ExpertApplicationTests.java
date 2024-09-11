c110adc6de182bb0588c1207a49f4709012623e3
MinJun Song <j34679@naver.com>
2024-09-11 14:34:48 +0900

    1-1

--- src/test/java/org/example/expert/ExpertApplicationTests.java
+++ src/test/java/org/example/expert/ExpertApplicationTests.java
@@ -2,12 +2,13 @@ package org.example.expert;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.assertj.core.api.Assertions;
 
 @SpringBootTest
 class ExpertApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
+
+
 
 }
